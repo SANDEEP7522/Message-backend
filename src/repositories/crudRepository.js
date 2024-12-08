7454d52922a8bc51393eab29f0c61255394c9d34
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2024-12-07 22:25:23 -0800

    Add crudRepository

--- src/repositories/crudRepository.js
+++ src/repositories/crudRepository.js
@@ -1,6 +1,5 @@
 export default function crudRepository(model) {
   return {
-
     create: async function (data) {
       const newDoc = await model.create(data);
       return newDoc;
@@ -10,17 +9,17 @@ export default function crudRepository(model) {
       const allDocs = await model.find();
       return allDocs;
     },
- 
+
     getById: async function (id) {
       const doc = await model.findById(id);
       return doc;
     },
- 
+
     delete: async function (id) {
       const response = await model.findByIdAndDelete(id);
       return response;
     },
- 
+
     update: async function (id, data) {
       const updatedDoc = await model.findByIdAndDelete(id, data, {
         new: true
--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -3,16 +3,16 @@ import crudRepository from './crudRepository.js';
 
 const userRepository = {
   ...crudRepository(User),
-  
+
   getByEmail: async function (email) {
     const user = await User.findOne({ email });
     return user;
   },
 
   getByUsername: async function (username) {
-     const user = await User.findOne({username})
-     return user;
+    const user = await User.findOne({ username });
+    return user;
   }
 };
 
-export default userRepository; 
+export default userRepository;
