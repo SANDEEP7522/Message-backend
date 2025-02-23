0d6c6cf24831c032a0abfef347808f58ff2fd223
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2025-02-23 15:10:19 +0530

    Payment capturePayment

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -14,16 +14,36 @@ export const createOrderController = async (req, res) => {
 
     const order = await razorpay.orders.create(options);
 
+    console.log('order...', order);
+
     if (!order) {
       throw new Error('Failed to cretated');
     }
 
     return res.status(StatusCodes.CREATED).json({
       success: true,
-      message: 'Odrer created successfully'
+      message: 'Odrer created successfully',
+      data: order
     });
   } catch (error) {
     console.log('Error in create Order Controller:', error);
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(internalErrorResponse);
+  }
+};
+
+export const capturePaymentController = async (req, res) => {
+  try {
+    console.log('Request params:', req.body);
+
+    return res.status(StatusCodes.OK).json({
+      Success: true,
+      message: 'Payment captured successfully',
+      data: payment
+    });
+  } catch (error) {
+    console.log('Error in capture payment Controller:', error);
     return res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
       .json(internalErrorResponse);
